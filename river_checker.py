70a09d1bdaffd9238d1241e44a872f97fc4f4ebe
annarailton <annarailton@github.com>
2020-02-21 11:40:02 +0000

    Add Isis flow rate calc

--- river_checker.py
+++ river_checker.py
@@ -30,11 +30,8 @@ C2T = {  # colour to terminal code
 POSSIBLE_FLAG_COLOURS = ['green', 'blue', 'orange', 'red', 'black', 'grey']
 
 
-def get_farmoor_data():
-    """Scrape the Farmoor gauge data
-
-    TODO get info from API
-    https://environment.data.gov.uk/flood-monitoring/id/stations/1100TH/readings?latest
+def get_farmoor_flow_rate():
+    """Get Farmoor flow rate data from EA API.
 
     Returns:
         str: flow rate (with unit)
@@ -83,8 +80,63 @@ def get_flag_data(reach):
     return colour, observation_datetime
 
 
+def get_isis_flow_rate():
+    """Calculate Isis flow rate using Osney and Iffley river level data from  EA API.
+
+  Returns:
+      str: flow rate (with unit)
+      str: colour (red, amber or green)
+      datetime.datetime: observation (calculation) date
+  """
+    def _get_reading(response):
+        """Get data from EA API"""
+        content = json.loads(response.content.decode('utf-8'))
+        level = content['items']['latestReading']['value']
+        observation_datetime = datetime.datetime.strptime(
+            content['items']['latestReading']['dateTime'],
+            '%Y-%m-%dT%H:%M:%SZ').replace(microsecond=0)
+
+        return level, observation_datetime
+
+    osney_id = '1303TH'
+    iffley_id = '1501TH'
+    upstream = 'stage'
+    downstream = 'downstage'
+
+    osney_downstream_level, osney_downstream_observation_datetime = _get_reading(
+        requests.get(
+            f'https://environment.data.gov.uk/flood-monitoring/id/measures/{osney_id}-level-{downstream}-i-15_min-mASD'
+        ))
+
+    iffley_upstream_level, iffley_downstream_observation_datetime = _get_reading(
+        requests.get(
+            f'https://environment.data.gov.uk/flood-monitoring/id/measures/{iffley_id}-level-{upstream}-i-15_min-mASD'
+        ))
+
+    # Pessimistically chose earliest time
+    observation_datetime = min(osney_downstream_observation_datetime,
+                               iffley_downstream_observation_datetime)
+
+    # Using Anu Dudhia formula
+    flow_rate = 100 * (osney_downstream_level - iffley_upstream_level - 2.07)
+
+    # Using Anu Dudhia levels take from graph on http://eodg.atm.ox.ac.uk/user/dudhia/rowing/river.html
+    if flow_rate >= 73:
+        colour = 'red'
+    elif 53 <= flow_rate < 73:
+        colour = 'amber'
+    elif 33 <= flow_rate < 53:
+        colour = 'blue'
+    else:
+        colour = 'green'
+
+    return f'{flow_rate:.3f}m^3/s', colour, observation_datetime
+
+
 if __name__ == '__main__':
-    farmoor_flow_rate, farmoor_colour, farmoor_time = get_farmoor_data()
+
+    farmoor_flow_rate, farmoor_colour, farmoor_time = get_farmoor_flow_rate()
+    isis_flow_rate, isis_colour, isis_time = get_isis_flow_rate()
 
     isis_flag, isis_flag_time = get_flag_data('isis')
     godstow_flag, godstow_flag_time = get_flag_data('godstow')
@@ -92,7 +144,9 @@ if __name__ == '__main__':
     print(
         f'Farmoor: {C2T[farmoor_colour]}{bcolors.BOLD}{farmoor_flow_rate}{bcolors.ENDC} at {farmoor_time}'
     )
-
+    print(
+        f'Isis: {C2T[isis_colour]}{bcolors.BOLD}{isis_flow_rate}{bcolors.ENDC} at {isis_time}'
+    )
     print(
         f'Isis flag: {C2T[isis_flag]}{bcolors.BOLD}{isis_flag}{bcolors.ENDC} at {isis_flag_time}'
     )
